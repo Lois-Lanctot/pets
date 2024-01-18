6295445e5b84d8f1b97d8e997f085aef91b40088
Lois Lanctot <lanctot.lois@student.greenriver.edu>
2024-01-18 12:29:52 -0500

    add home page view

--- index.php
+++ index.php
@@ -20,9 +20,8 @@ $f3 = Base::instance();
 //Define a default route
 $f3->route('GET /', function () {
 
-    echo "<p>Pet Home</p>";
-//    $view = new Template();
-//    echo $view->render('views/home.html');
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
 //Run Fat-Free
