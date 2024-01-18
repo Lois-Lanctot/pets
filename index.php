f3c332547183999f6ece79f436f9c5e62e2f1e85
Lois Lanctot <lanctot.lois@student.greenriver.edu>
2024-01-18 12:26:32 -0500

    added controller and .gitignore

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * Lois Lanctot
+ * January 2024
+ * https://lois.greenriverdev.com/328/pets/
+ * This is my CONTROLLER for the pets pair program
+ */
+
+//Turn on error reporting
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Instantiate F3
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /', function () {
+
+    echo "<p>Pet Home</p>";
+//    $view = new Template();
+//    echo $view->render('views/home.html');
+});
+
+//Run Fat-Free
+$f3->run();
